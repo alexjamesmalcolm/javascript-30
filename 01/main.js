5b1c2b88ba284485994975779858813b9ddd4831
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2019-09-05 17:09:17 -0400

    Cleaned up UI

--- 01/main.js
+++ 01/main.js
@@ -10,12 +10,16 @@ window.onload = () => {
   console.timeEnd("document ready");
   const soundsElement = document.querySelector("#sounds");
   sounds.forEach(sound => {
+    new Audio(sound.source);
     const playSound = () => {
       const audioElement = new Audio(sound.source);
+      audioElement.volume = 0.25;
       audioElement.play();
+      buttonElement.classList.add("active");
+      setTimeout(() => buttonElement.classList.remove("active"), 1000);
     };
     const buttonElement = el(
-      `<button><p>${sound.name}</p><p>key: ${sound.key}</p></button>`
+      `<button><p class="button-key">${sound.key}</p><p class="button-description">${sound.name}</p></button>`
     );
     soundsElement.appendChild(buttonElement);
     buttonElement.onclick = playSound;
