b379ab1ffbf1973b31780c7d382bc678267f798e
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2019-09-08 07:09:59 -0400

    Finished coding

--- 03/main.js
+++ 03/main.js
@@ -1,1 +1,28 @@
-window.onload = () => {};
+window.onload = () => {
+  const data = [
+    {
+      inputSelector: "#spacing",
+      cssVar: "--spacing",
+      formatter: value => value + "px"
+    },
+    {
+      inputSelector: "#blur",
+      cssVar: "--blur",
+      formatter: value => value + "px"
+    },
+    {
+      inputSelector: "#border-color",
+      cssVar: "--border-color",
+      formatter: value => value
+    }
+  ];
+  const root = document.documentElement;
+  const updateValue = (cssVar, formatter) => event => {
+    const value = event.target.value;
+    root.style.setProperty(cssVar, formatter(value));
+  };
+  data.forEach(({ inputSelector, cssVar, formatter }) => {
+    const input = document.querySelector(inputSelector);
+    input.addEventListener("change", updateValue(cssVar, formatter));
+  });
+};
